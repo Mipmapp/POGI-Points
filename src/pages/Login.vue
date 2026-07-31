b8e125f95cb8078dab49d22dad02a88757d206a7
Mipmapp <157580621+Mipmapp@users.noreply.github.com>
2026-07-31 05:06:31 +0000

    PUSH

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -595,7 +595,7 @@
           <button type="button" @click="goToRegister" class="text-indigo-500 hover:text-indigo-700 font-semibold ml-1 transition">Sign up</button>
         </p>
 
-        <div class="mt-6 text-center text-xs text-gray-400">
+        <div class="mt-2 text-center text-xs text-gray-400">
           Powered by <button @click="showDevelopersPopup = true" class="text-blue-400 font-medium hover:text-blue-600 cursor-pointer transition">CCS - Creatives Committee</button>
         </div>
       </div>
@@ -692,7 +692,7 @@
           <button type="button" @click="goToRegister" class="text-indigo-500 hover:text-indigo-700 font-semibold ml-1 transition">Sign up</button>
         </p>
 
-        <div class="mt-6 text-center text-xs text-gray-400">
+        <div class="mt-2 text-center text-xs text-gray-400">
           Powered by <button @click="showDevelopersPopup = true" class="text-blue-400 font-medium hover:text-blue-600 cursor-pointer transition">CCS - Creatives Committee</button>
         </div>
         <div class="mt-2 text-center text-xs text-gray-400">
